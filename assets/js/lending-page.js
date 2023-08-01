d59fb68e2de6a3ee72bd8b484da3335707e3ca77
calutol <110926906+calutol@users.noreply.github.com>
2023-08-01 02:59:13 -0400

    add new

---
+++ assets/js/lending-page.js
@@ -0,0 +1,7 @@
+// Placeholder
+function miFuncion() {
+    // Código que deseas ejecutar una vez que la página se haya cargado completamente
+    $(".placeholder").removeClass('placeholder')
+    $(".color-loader").removeClass('color-loader')
+    $(".bg-loader").removeClass('bg-loader')
+}
