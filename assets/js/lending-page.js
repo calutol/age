85aeb96fb4ef82edf1d65be4ca26a90c89525c90
calutol <110926906+calutol@users.noreply.github.com>
2023-10-23 12:17:36 -0400

    rup

--- assets/js/lending-page.js
+++ assets/js/lending-page.js
@@ -6,3 +6,16 @@ function miFuncion() {
     $(".bg-loader").removeClass('bg-loader')
     $(".border-loader").removeClass('border-loader')
 }
+
+function irArriba(pxPantalla) {
+    window.addEventListener('scroll', () => {
+        var scroll = document.documentElement.scrollTop;
+        var buttonUp = document.getElementById('buttonUp');
+        if (scroll > pxPantalla) {
+            buttonUp.style.right = 0 + "px";
+        } else {
+            buttonUp.style.right = -100 + "px";
+        }
+    })
+}
+irArriba(2300); 
