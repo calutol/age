809afab59cbdd7c0d069212dc7f33e3f835abaad
calutol <110926906+calutol@users.noreply.github.com>
2023-10-27 14:17:49 -0400

    nuevo

--- assets/js/lending-page.js
+++ assets/js/lending-page.js
@@ -46,3 +46,13 @@ function updateCountdown() {
 
 // Iniciar la cuenta regresiva
 updateCountdown();
+
+// Galeria
+document.addEventListener("click", function (e) {
+    if (e.target.classList.contains("gallery-item")) {
+        const src = e.target.getAttribute("src");
+        document.querySelector(".modal-img").src = src;
+        const myModal = new bootstrap.Modal(document.getElementById('gallery-popup'));
+        myModal.show();
+    }
+})
